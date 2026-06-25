0228be1423f0952880c0094be415f000e0e23e4d
betofortes <mateus.bittar.fortes@gmail.com>
2026-06-25 19:29:37 -0300

    Atualiza cache do Workout v12

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "workout-shell-v11";
+const CACHE_NAME = "workout-shell-v12";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=11",
-  "./app.js?v=11",
-  "./manifest.webmanifest?v=11",
+  "./styles.css?v=12",
+  "./app.js?v=12",
+  "./manifest.webmanifest?v=12",
   "./icons/icon.svg?v=7",
 ];
 
